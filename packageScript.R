05704d0f1295d28abdd0d9c9b021e04e84a8d28f
懒麻蛇 <frueddy@gmail.com>
2024-05-29 09:10:21 +0800

    gh-page

--- packageScript.R
+++ packageScript.R
@@ -47,8 +47,8 @@ usethis::use_vignette('brainscore')
 usethis::use_pkgdown()
 usethis::edit_r_environ()
 usethis::browse_github_pat()
-
-usethis::use_pkgdown_github_pages()
 pkgdown::build_site()
+usethis::use_pkgdown_github_pages()
+
 # prepare example data
 
